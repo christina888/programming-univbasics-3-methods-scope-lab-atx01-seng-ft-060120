a571d43df97e1450d3db274dc72f36b836d5557e
Christina Chianelli <cechianelli@gmail.com>
2020-05-20 01:32:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,16 +1,16 @@
 def mario
   phrase = "It's-a me, Mario!\n"
-  return phrase
+  puts phrase
 end
 
 def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!\n"
-  return status
+  puts status
 end
 
 def link
   advice = "It's Dangerous to Go Alone! Take This.\n"
-  return advice 
+  puts advice 
 end
 
 def all_phrases
