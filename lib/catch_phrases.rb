6b1b3579e35ab06256bfe037931bc68066b13cd2
Christina Chianelli <cechianelli@gmail.com>
2020-05-20 01:33:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,15 +1,15 @@
 def mario
-  phrase = "It's-a me, Mario!\n"
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
-  status = "Thank You Mario! But Our Princess Is In Another Castle!\n"
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
 
 def link
-  advice = "It's Dangerous to Go Alone! Take This.\n"
+  advice = "It's Dangerous to Go Alone! Take This."
   puts advice 
 end
 
